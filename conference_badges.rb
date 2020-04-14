7f4fb6863378d01b651792467c7faa7c3825b69e
Juan Cobian <cobianjuan19@gmail.com>
2020-04-14 23:12:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,11 @@ def assign_rooms(attendees)
   end
 end
 
-def printer
-  print batch_badge_creator
-  print assign_rooms
-end 
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
