b4abaa6cef0cad32c25657c884af468693b42d11
Juan Cobian <cobianjuan19@gmail.com>
2020-04-14 23:10:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,9 +9,13 @@ def batch_badge_creator(attendees)
   end
 end
 
-def assign_rooms(speaker, room)
-  "Hello, #{}! You'll be assigned to room #{}!"
-end 
+def assign_rooms(attendees)
+  room=0
+  attendees.collect do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
 
 def printer
   print batch_badge_creator
