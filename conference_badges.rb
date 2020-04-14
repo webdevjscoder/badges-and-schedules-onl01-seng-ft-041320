a9b83092f8e24e032b805da36f60a3601a20e76a
Juan Cobian <cobianjuan19@gmail.com>
2020-04-14 23:03:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,9 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  array.each do |speakers|
-    
+  array = 
 end
 
 def assign_rooms(speaker, room)
